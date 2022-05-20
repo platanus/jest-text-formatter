13a60882ee50050a2c729569414ca9ad69f1583c
FloValdes <ftvaldes1@uc.cl>
2022-05-20 14:30:40 -0400

    feat(jest-text-formatter): add formatter for jest coverage outputs

---
+++ app/jest-text-formatter.spec.js
@@ -0,0 +1,35 @@
+const { processLine } = require('./jest-text-formatter');
+const fs = require("fs");
+
+jest.mock('fs');
+
+describe('process line', () => {
+  const currentPath = 'current/path';
+  const outputPath = 'output.txt';
+
+  it('does not format invalid line', () => {
+    expect(processLine('Invalid line --', currentPath, outputPath)).toBe(currentPath);
+  });
+
+  it('correctly formats folder path', () => {
+    const folderLine = ' app-name/app/new/path |     100 |      100 |     100 |     100 |';
+    expect(processLine(folderLine, currentPath, outputPath)).toBe('app/new/path');
+  });
+
+  it('correctly formats file line with coverage lines', async () => {
+    const filePathCoverage = '  file.js |       0 |        0 |       0 |       0 | 11-69,78';
+    processLine(filePathCoverage, currentPath, outputPath);
+    expect(fs.writeFileSync).toHaveBeenCalledTimes(2);
+    expect(fs.writeFileSync).toHaveBeenNthCalledWith(
+      1, outputPath, '/current/path/file.js:11:1: Not covered lines: 11 to 69\n', { flag: 'a' }
+    );
+    expect(fs.writeFileSync).toHaveBeenNthCalledWith(
+      2, outputPath, '/current/path/file.js:78:1: Not covered lines: 78\n', { flag: 'a' }
+    );
+  });
+
+  it('does not format file line without coverage', () => {
+    const filePathNoCoverage = '  file.js |       0 |        0 |       0 |       0 | ';
+    expect(processLine(filePathNoCoverage, currentPath, outputPath)).toBe(currentPath);
+  });
+});
